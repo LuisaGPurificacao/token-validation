f98d10cea3c6f9bd0a73641dbc947fd670929cea
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2024-05-07 21:06:49 -0300

    feat: initial project

---
+++ src/test/java/br/com/jwt/jwtvalidation/JwtValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jwt.jwtvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
