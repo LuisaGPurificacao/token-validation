2fe498b61ce3f2a3aa0cbe4b497fc87934b25326
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2024-05-12 10:30:22 -0300

    fix: removing mock from integration tests

--- src/test/java/br/com/jwt/validation/app/resource/AuthenticationResourceITest.java
+++ src/test/java/br/com/jwt/validation/app/resource/AuthenticationResourceITest.java
@@ -1,59 +1,44 @@
 package br.com.jwt.validation.app.resource;
 
-import br.com.jwt.validation.app.dto.request.AuthenticationRequest;
-import br.com.jwt.validation.app.service.IAuthenticationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static br.com.jwt.validation.fixture.AuthenticationFixture.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class AuthenticationResourceTest {
+class AuthenticationResourceITest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private IAuthenticationService service;
-
     @Test
     void testValidateTokenSuccess() throws Exception {
-        when(service.validateToken(any(AuthenticationRequest.class))).thenReturn(getValidResponse());
-
         mockMvc.perform(post("/validate-token")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(new ObjectMapper().writeValueAsString(getValidRequest())))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.isTokenValid").value(true))
                 .andExpect(jsonPath("$.errorDescription").doesNotExist());
-
-        verify(service, times(1)).validateToken(any(AuthenticationRequest.class));
     }
 
     @Test
     void testValidateTokenFail() throws Exception {
-        when(service.validateToken(any(AuthenticationRequest.class))).thenReturn(getInvalidResponse());
-
         mockMvc.perform(post("/validate-token")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(new ObjectMapper().writeValueAsString(getValidRequest())))
+                        .content(new ObjectMapper().writeValueAsString(getInvalidPayloadRequest())))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.isTokenValid").value(false))
-                .andExpect(jsonPath("$.errorDescription").value("error"));
-
-        verify(service, times(1)).validateToken(any(AuthenticationRequest.class));
+                .andExpect(jsonPath("$.errorDescription").value("JWT payload is invalid"));
     }
 
 }
--- src/test/java/br/com/jwt/validation/fixture/AuthenticationFixture.java
+++ src/test/java/br/com/jwt/validation/fixture/AuthenticationFixture.java
@@ -29,13 +29,6 @@ public class AuthenticationFixture {
                 .build();
     }
 
-    public static AuthenticationResponse getInvalidResponse() {
-        return AuthenticationResponse.builder()
-                .isTokenValid(false)
-                .errorDescription("error")
-                .build();
-    }
-
     public static UserEntity getUserEntity() {
         return UserEntity.builder()
                 .name("Maria Oliveira")
